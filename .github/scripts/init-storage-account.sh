4a6cd197ad779f332400b1531d90b009345ac8e3
rimlaban7 <a.banjanovic@gmail.com>
2024-03-14 12:02:19 -0400

    Add init-storage-account.yml workflow file.

--- .github/scripts/init-storage-account.sh
+++ .github/scripts/init-storage-account.sh
@@ -16,10 +16,10 @@ echo "$STORAGE_ACCOUNT_FULL_NAME"
 echo "$CONTAINER_NAME"
 
 # Create resource group
-#az group create --name $RESOURCE_GROUP_FULL_NAME --location {{ secret.REGION }}
+az group create --name $RESOURCE_GROUP_FULL_NAME --location {{ secret.REGION }}
 
 # Create storage account
-#az storage account create --resource-group $RESOURCE_GROUP_FULL_NAME --name $STORAGE_ACCOUNT_FULL_NAME --sku $STORAGE_ACCOUNT_SKU --encryption-services blob
+az storage account create --resource-group $RESOURCE_GROUP_FULL_NAME --name $STORAGE_ACCOUNT_FULL_NAME --sku $STORAGE_ACCOUNT_SKU --encryption-services blob
 
 # Create blob container
-#az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
