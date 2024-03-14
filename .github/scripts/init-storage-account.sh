42e776f58302eede0f72b9cef35a8717407e6920
rimlaban7 <a.banjanovic@gmail.com>
2024-03-14 11:03:09 -0400

    Parameterize init-storage-account.sh

--- .github/scripts/init-storage-account.sh
+++ .github/scripts/init-storage-account.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-RESOURCE_GROUP_NAME=rg-
-STORAGE_ACCOUNT_NAME=tfstate$RANDOM
-CONTAINER_NAME=tfstate
+RESOURCE_GROUP_NAME="${{ env.RESOURCE_GROUP_NAME}}${{ env.ENVIRONMENT_NAME }}"
+STORAGE_ACCOUNT_NAME="${{ env.STORAGE_ACCOUNT_NAME }}${{ env.ENVIRONMENT_NAME }}$RANDOM"
+CONTAINER_NAME="${{ env.TERRAFORM_STATE }}${{ env.ENVIRONMENT_NAME }}"
 
 # Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location eastus
+az group create --name $RESOURCE_GROUP_NAME --location {{ env.REGION }}
 
 # Create storage account
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku {{ env.STORAGE_ACCOUNT_SKU }} --encryption-services blob
 
 # Create blob container
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
