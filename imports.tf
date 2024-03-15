b9e76976d8decd104d6fdee9ca66b0cdbb9b4f69
rimlaban7 <a.banjanovic@gmail.com>
2024-03-15 11:15:01 -0400

    Add initial Terraform files.

--- imports.tf
+++ imports.tf
@@ -1,4 +1,9 @@
 import {
-  to = storage_account.storage_account
+  to = azurerm_resource_group.resource_group
+  id = var.resource_group_import_id
+}
+
+import {
+  to = azurerm_storage_account.storage_account
   id = var.storage_account_import_id
 }
