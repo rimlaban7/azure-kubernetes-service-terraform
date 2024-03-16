0f4d18c5711872f126fbaea16e0eac290b70c9e5
rimlaban7 <a.banjanovic@gmail.com>
2024-03-16 16:24:03 -0400

    Modify init-remote-backeynd.yml to use environment secrets.

--- modules/storage_account/outputs.tf
+++ modules/storage_account/outputs.tf
@@ -1,7 +1,3 @@
 output "storage_account_name" {
     value = azurerm_storage_account.name
 }
-
-output "primary_access_key" {
-    value = azurerm_storage_account.storage_account.primary_access_key
-}
--- variables.tf
+++ variables.tf
@@ -1,25 +1,21 @@
 variable "environment" {
   type = string
   sensitive = true
-  default = "prod"
 }
 
 variable "resource_group_name" {
     type = string
     sensitive = true
-    default = "terra_cloud_github_actions_rg"
 }
 
 variable "location" {
   type = string
   sensitive = true
-  default = "East Us 2"
 }
 
 variable "storage_account_prefix" {
     type = string
     sensitive = true
-    default = "terracloudgithub"
 }
 
 variable "storage_account_import_id" {
