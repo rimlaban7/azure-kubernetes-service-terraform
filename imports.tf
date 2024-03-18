acb93c868400dd3ee646f98194a189161e50211f
rimlaban7 <a.banjanovic@gmail.com>
2024-03-18 10:11:04 -0400

    Modify imports.tf

--- imports.tf
+++ imports.tf
@@ -2,9 +2,8 @@ import {
   to = module.resource_group.azurerm_resource_group.resource_group
   id = var.resource_group_import_id
 }
-/*
+
 import {
-  to = azurerm_storage_account.storage_account
+  to = module.storage_account.azurerm_storage_account.storage_account
   id = var.storage_account_import_id
 }
-*/
