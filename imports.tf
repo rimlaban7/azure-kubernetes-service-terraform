06b040646f7d7a766aa452d9bc25bb482aa2aedf
rimlaban7 <a.banjanovic@gmail.com>
2024-03-17 21:58:41 -0400

    Modify deploy-infra.yml

--- imports.tf
+++ imports.tf
@@ -1,9 +1,10 @@
 import {
-  to = azurerm_resource_group.resource_group
+  to = module.resource_group.azurerm_resource_group.resource_group
   id = var.resource_group_import_id
 }
-
+/*
 import {
   to = azurerm_storage_account.storage_account
   id = var.storage_account_import_id
 }
+*/
