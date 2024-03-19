f6e02e010077fbb77398b1c8e4401e5e3751893b
rimlaban7 <a.banjanovic@gmail.com>
2024-03-19 18:46:24 -0400

    Modify imports.tf

--- imports.tf
+++ imports.tf
@@ -1,9 +1,9 @@
 import {
-  to = module.resource_group.azurerm_resource_group.resource_group
+  to = azurerm_resource_group.resource_group
   id = var.resource_group_import_id
 }
 
 import {
-  to = module.storage_account.azurerm_storage_account.storage_account
+  to = azurerm_storage_account.storage_account
   id = var.storage_account_import_id
 }
