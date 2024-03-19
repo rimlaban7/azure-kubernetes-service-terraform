00aa016214f0d293001a848524d05e4f84687eb1
rimlaban7 <a.banjanovic@gmail.com>
2024-03-19 19:53:59 -0400

    Modify main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 locals {
     full_resource_group_name = format("rg-%s-%s", var.resource_group_name, var.environment)
     full_storage_account_name = format("st%s%s", var.storage_account_name, var.environment)
-    full_virtual_network_name = format("vnet-%s-%s", var.virtual_network_name, var.environment)
 }
 
 resource "azurerm_resource_group" "resource_group" {
@@ -23,49 +22,6 @@ resource "azurerm_storage_account" "storage_account" {
   allow_nested_items_to_be_public = false
   cross_tenant_replication_enabled = false
   min_tls_version = "TLS1_2"
-
-  tags = {
-    environment = var.environment
-  }
-}
-
-resource "azurerm_virtual_network" "virtual_network" {
-  name                = local.full_virtual_network_name
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  address_space       = ["10.0.0.0/20"]
-
-  subnet = {
-    name = "subnet_1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  tags = {
-    environment = var.environment
-  }
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "PublicIPForLB"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  allocation_method   = "Static"
-  sku = "Standard"
-
-  tags = {
-    environment = var.environment
-  }
-}
-
-resource "azurerm_lb" "lb" {
-  name                = "LoadBalancer"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.public_ip.id
-  }
 
   tags = {
     environment = var.environment
