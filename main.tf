46005ab4aabe03d260c82ffb0b66bd504bf87d0d
rimlaban7 <a.banjanovic@gmail.com>
2024-03-19 18:40:46 -0400

    Modify main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = var.storage_account_name
+  name                     = local.full_storage_account_name
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = var.account_tier
