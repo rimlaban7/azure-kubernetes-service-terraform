48bb1ff69c3de171cebd69afb90f64b92d964547
rimlaban7 <a.banjanovic@gmail.com>
2024-03-19 19:09:21 -0400

    Modify main.tf

--- main.tf
+++ main.tf
@@ -52,6 +52,11 @@ resource "azurerm_virtual_network" "virtual_network" {
   resource_group_name = azurerm_resource_group.resource_group.name
   address_space       = ["10.0.0.0/20"]
 
+  subnet = {
+    name = "subnet_1"
+    address_prefix = "10.0.1.0/24"
+  }
+
   tags = {
     environment = var.environment
   }
