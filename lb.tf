be7e3b66d049ad55b3523ac37f0304bdc95e3b48
rimlaban7 <a.banjanovic@gmail.com>
2024-03-20 12:02:37 -0400

    Add lb.tf

--- lb.tf
+++ lb.tf
@@ -40,3 +40,41 @@ resource "azurerm_lb_nat_pool" "lb_nat_pool" {
     backend_port                   = 22
     frontend_ip_configuration_name = "PublicIPAddress"
 }
+
+resource "azurerm_lb_probe" "lb_probe" {
+    loadbalancer_id = azurerm_lb.lb.id
+    name = "http-probe"
+    protocol = "Http"
+    request_path = "/"
+    port = 80
+}
+
+resource "azurerm_lb_rule" "lb_rule" {
+  loadbalancer_id                = azurerm_lb.lb.id
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 3389
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_lb_outbound_rule" "lb_outbound_rule" {
+  name                    = "OutboundRule"
+  loadbalancer_id         = azurerm_lb.lb.id
+  protocol                = "Tcp"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.lb_backend_address_pool.id
+
+  frontend_ip_configuration {
+    name = "PublicIPAddress"
+  }
+}
+
+resource "azurerm_lb_nat_rule" "example" {
+  resource_group_name            = azurerm_resource_group.resource_group.name
+  loadbalancer_id                = azurerm_lb.lb.id
+  name                           = "RDPAccess"
+  protocol                       = "Tcp"
+  frontend_port                  = 3389
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
